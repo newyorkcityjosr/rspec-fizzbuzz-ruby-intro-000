4f78f0e48587ea63e2e2934c00f3c581d0c1c190
jose tavarez <newyorkcityjose@gmail.com>
2019-11-15 00:08:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,4 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
+  if (number % 3 == 0) && (number % 5 == 0)
+    "FizzBuzz"
+  elsif number % 5 == 0
+    "Buzz"
+  elsif (number % 3 == 0)
+    "Fizz"
+  end
 end
